6090f4c3969fe90d0af641eecd93e704f2192418
Cyrus Yncierto <mark.yncierto@rockpress.com.au>
2023-07-19 14:57:35 +0800

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Link from "next/link";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component }: AppProps) {
+  return (
+    <div>
+      <div style={{ display: "flex", gap: "12px" }}>
+        <Link href="/">Home</Link>
+        <Link href="/twist">Twist</Link>
+        <Link href="/resources">Resources</Link>
+      </div>
+      <Component />
+    </div>
+  );
 }
---
+++ src/pages/twist/index.tsx
@@ -0,0 +1,7 @@
+export default function Twist() {
+    return (
+      <div>
+        <h1>Twist</h1>
+      </div>
+    );
+}
